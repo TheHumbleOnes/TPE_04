5e854cf1c172791fedfa95ed9a432d4020bdaa11
Axel Duenninger <axelduenninger@gmail.com>
2014-06-15 19:44:14 +0200

    Saal getter/setter angepasst

--- TPE_04/src/de/hs_mannheim/tpe/imb/ss14/g02/uebung4/Saal.java
+++ TPE_04/src/de/hs_mannheim/tpe/imb/ss14/g02/uebung4/Saal.java
@@ -1,27 +1,32 @@
 package de.hs_mannheim.tpe.imb.ss14.g02.uebung4;
 
-import java.util.HashMap;
 
-import org.omg.CosNaming.NamingContextExtPackage.AddressHelper;
 
 public class Saal {
     private String name;
     private int seats;
-    
-  
-    public Saal(String name, int seats){
-    	this.name = name;
-    	this.seats = seats;
+
+    public Saal(String name, int seats) {
+        this.setName(name);
+        this.setSeats(seats);
     }
 
-	public int getSeat() {
-		return seats;
-	}
+    public int getSeats() {
+        return seats;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
+    private void setName(String name) {
+        this.name = name;
+    }
 
+    private void setSeats(int seats) {
+        this.seats = seats;
+    }
+    
+    
 
 }

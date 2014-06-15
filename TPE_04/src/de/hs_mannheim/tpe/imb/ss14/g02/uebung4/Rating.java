83580c4e08041d2b66565351ce1eeffb7d0b6e4c
Axel Duenninger <axelduenninger@gmail.com>
2014-06-15 19:49:30 +0200

    Rating angepasst

--- TPE_04/src/de/hs_mannheim/tpe/imb/ss14/g02/uebung4/Rating.java
+++ TPE_04/src/de/hs_mannheim/tpe/imb/ss14/g02/uebung4/Rating.java
@@ -2,22 +2,28 @@ package de.hs_mannheim.tpe.imb.ss14.g02.uebung4;
 
 public enum Rating {
 
-   AB_0("Ohne Altersbeschraenkung"),
-   AB_6("Ab 6 Jahren"),
-   AB_12("Ab 12 Jahren"),
-   AB_16("Ab 16 Jahren"),
-   AB_18("Ohne Jugendfreigabe");
-   
-   private final String freigabe;
-   
-   private Rating(final String freigabe){
-       this.freigabe = freigabe;
-   }
-   
-   @Override
-   public String toString(){
-       return freigabe;
-   }
-        
-    
+    AB_0("Ohne Altersbeschraenkung"),
+    AB_6("Ab 6 Jahren"),
+    AB_12("Ab 12 Jahren"),
+    AB_16("Ab 16 Jahren"),
+    AB_18("Ohne Jugendfreigabe");
+
+    private String freigabe;
+
+    private Rating(String freigabe) {
+        this.setRating(freigabe);
+    }
+
+    public String getRating() {
+        return freigabe;
+    }
+
+    private void setRating(String freigabe) {
+        this.freigabe = freigabe;
+    }
+
+    public String toString() {
+        return this.freigabe;
+    }
+
 }

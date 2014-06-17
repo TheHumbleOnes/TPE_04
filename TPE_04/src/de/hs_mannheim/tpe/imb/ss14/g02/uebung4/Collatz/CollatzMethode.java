b6d6bfc538d7a04385174f6d6d43ebb45d9a98af
Axel Duenninger <axelduenninger@gmail.com>
2014-06-17 18:55:30 +0200

    Nur noch eine Zeile mit Ausgabe der länsgten Folge mit zugehöriger Zahl

--- TPE_04/src/de/hs_mannheim/tpe/imb/ss14/g02/uebung4/Collatz/CollatzMethode.java
+++ TPE_04/src/de/hs_mannheim/tpe/imb/ss14/g02/uebung4/Collatz/CollatzMethode.java
@@ -1,68 +1,49 @@
 package de.hs_mannheim.tpe.imb.ss14.g02.uebung4.Collatz;
 
-
-
 public class CollatzMethode implements Runnable {
 
-	private long start;
-	private long ende;
-	Long max = 0L;
-	Long iterierungen = 0L;
-	
+    private long start;
+    private long ende;
 
-	public CollatzMethode(long start, long ende) {
-		this.start = start;
-		this.ende = ende;
-	}
+    static Long iterierungen = 0L;
 
-	public void run() {
+    public CollatzMethode(long start, long ende) {
+        this.start = start;
+        this.ende = ende;
+    }
 
-				
+    public void run() {
 
-		for ( long i = this.start; i <= this.ende; i++) {
+        for (long i = this.start; i <= this.ende; i++) {
 
-			long temp = i;
-			long collatz = 0;
-			while (temp != 1) {
-				if (temp % 2 == 0) {
-					temp /= 2;
-					collatz++;
-				} else {
-					temp = ((3 * temp) + 1);
-					collatz++;
-				}
-			}
-			
-			maxLength(i, collatz); 
-			
-		}
-		System.out.println("Längste Folge mit " + iterierungen + " Iterationen bei Zahl " + max);
-		
-		
-//		Set<Map.Entry<Long, Long>> entrySet = map.entrySet();
-//		long max = 0;
-//		long index = 0;
-//		for (Map.Entry<Long, Long> entry : entrySet) {
-//			long key = entry.getKey();
-//			long value = entry.getValue();
-//			if (value > max) {
-//				max = value;
-//				index = key;
-//			}
-//		}
-//		System.out.println(index + " " + max);
-	}
-	
-	private void maxLength(Long i, Long collatz){
-		
-		if(collatz > iterierungen){
-			iterierungen = collatz;
-			max = i;
-		}
-		
-	}
-	public String toString(){
-		return "Längste Folge mit " + iterierungen + " Iterationen ist bei Zahl " + max;
-	}
+            long temp = i;
+            long collatz = 0;
+            while (temp != 1) {
+                if (temp % 2 == 0) {
+                    temp /= 2;
+                    collatz++;
+                } else {
+                    temp = ((3 * temp) + 1);
+                    collatz++;
+                }
+            }
+
+            maxLength(i, collatz);
+
+        }
+        // System.out.println("Längste Folge mit " + iterierungen +
+        // " Iterationen bei Zahl " + max);
+
+    }
+
+    private void maxLength(Long i, Long collatz) {
+
+        if (collatz > iterierungen) {
+            iterierungen = collatz;
+            CollatzThread.maxIterations = collatz;
+            CollatzThread.number = i;
+        }
+
+    }
 
 }
--- TPE_04/src/de/hs_mannheim/tpe/imb/ss14/g02/uebung4/Collatz/CollatzThread.java
+++ TPE_04/src/de/hs_mannheim/tpe/imb/ss14/g02/uebung4/Collatz/CollatzThread.java
@@ -2,26 +2,33 @@ package de.hs_mannheim.tpe.imb.ss14.g02.uebung4.Collatz;
 
 public class CollatzThread {
 
-	public static void main(String[] args) throws InterruptedException {
+    public static Long number;
+    public static Long maxIterations;
 
-		CollatzMethode c1 = new CollatzMethode(1, 250000);
-		CollatzMethode c2 = new CollatzMethode(250001, 500000);
-		CollatzMethode c3 = new CollatzMethode(500001, 750000);
-		CollatzMethode c4 = new CollatzMethode(750001, 1000000);
-		
-		Thread t1 = new Thread(c1);
-		Thread t2 = new Thread(c2);
-		Thread t3 = new Thread(c3);
-		Thread t4 = new Thread(c4);
-		
-		t1.start();
-		t2.start();
-		t3.start();
-		t4.start();
-		
-//		t1.join();
-//		t2.join();
-//		t3.join();
-//		t4.join();
-	}
+    public static void main(String[] args) throws InterruptedException {
+
+        CollatzMethode c1 = new CollatzMethode(1, 250000);
+        CollatzMethode c2 = new CollatzMethode(250001, 500000);
+        CollatzMethode c3 = new CollatzMethode(500001, 750000);
+        CollatzMethode c4 = new CollatzMethode(700001, 1000000);
+
+        Thread t1 = new Thread(c1);
+        Thread t2 = new Thread(c2);
+        Thread t3 = new Thread(c3);
+        Thread t4 = new Thread(c4);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+
+        System.out.println("Längste Folge mit " + maxIterations + " bei Zahl "
+                + number);
+
+    }
 }

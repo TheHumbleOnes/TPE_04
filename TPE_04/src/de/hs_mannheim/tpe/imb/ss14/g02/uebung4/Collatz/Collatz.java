64f0775c9a1415ef518f61ab445f9c69bd096af4
Michelle <michelle.heiler@web.de>
2014-06-17 15:42:16 +0200

    hier noch mit der dummen Collatz folge

---
+++ TPE_04/src/de/hs_mannheim/tpe/imb/ss14/g02/uebung4/Collatz/Collatz.java
@@ -0,0 +1,56 @@
+package de.hs_mannheim.tpe.imb.ss14.g02.uebung4.Collatz;
+
+import java.util.*;
+
+public class Collatz {
+	static ArrayList<Long> list = new ArrayList<Long>();
+
+	public static void main(String[] args) {
+
+		coll(5);
+		Iterator<Long> i = new Iterator<Long>() {
+
+			private int counter = 0;
+
+			@Override
+			public boolean hasNext() {
+				if (counter < list.size()) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+
+			@Override
+			public Long next() {
+				return list.get(counter++);
+			}
+
+			@Override
+			public void remove() {
+				list.remove(counter);
+			}
+		};
+	}
+
+	public static void coll(long startwert) {
+
+		long temp = startwert;
+		while (temp != 1) {
+			if (temp % 2 == 0) {
+				temp /= 2;
+				list.add(temp);
+			} else {
+				temp = ((3 * temp) + 1);
+				list.add(temp);
+			}
+		}
+		final ListIterator<Long> iterator = list.listIterator();
+
+		while (iterator.hasNext()) {
+			long wert = iterator.next();
+			System.out.print(wert + ",");
+		}
+	}
+
+}

0fd2fdadae62835ca7534faf138ac81b56f5a28b
Axel Duenninger <axelduenninger@gmail.com>
2014-06-17 19:19:36 +0200

    Noch was kosmetisches...

--- TPE_04/src/de/hs_mannheim/tpe/imb/ss14/g02/uebung4/Collatz/CollatzThread.java
+++ TPE_04/src/de/hs_mannheim/tpe/imb/ss14/g02/uebung4/Collatz/CollatzThread.java
@@ -7,10 +7,10 @@ public class CollatzThread {
 
     public static void main(String[] args) throws InterruptedException {
 
-        CollatzMethode c1 = new CollatzMethode(1, 25000000);
-        CollatzMethode c2 = new CollatzMethode(25000001, 50000000);
-        CollatzMethode c3 = new CollatzMethode(50000001, 75000000);
-        CollatzMethode c4 = new CollatzMethode(70000001, 100000000);
+        CollatzMethode c1 = new CollatzMethode(1, 2500000);
+        CollatzMethode c2 = new CollatzMethode(25000001, 500000000);
+        CollatzMethode c3 = new CollatzMethode(5000001, 7500000);
+        CollatzMethode c4 = new CollatzMethode(7000001, 10000000);
 
         Thread t1 = new Thread(c1);
         Thread t2 = new Thread(c2);
@@ -27,8 +27,8 @@ public class CollatzThread {
         t3.join();
         t4.join();
 
-        System.out.println("Längste Folge mit " + maxIterations + " bei Zahl "
-                + number);
+        System.out.println("Längste Folge mit " + maxIterations
+                + " Iterationen bei Zahl " + number);
 
     }
 }
